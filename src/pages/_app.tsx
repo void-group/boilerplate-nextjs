9b3652d0f6866bc6f602b70b3496eb7afa146cda
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2021-10-02 14:00:56 -0300

    chore: improve SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
+import { globalStyles } from '@/styles/defaultStyles/globalStyles';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-
-import { globalStyles } from '../styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   globalStyles();
   return (
     <>
       <Head>
-        <title>PWA - Boilerplate</title>
+        <title>Boilerplate VOID</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="title" content="Boilerplate VOID" />
+        <meta
+          name="description"
+          content="Um boilerplate criado com nextjs, typescript, stitches, pwa e muito mais"
+        />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Portuguese" />
       </Head>
       <Component {...pageProps} />
     </>
